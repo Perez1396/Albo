60741b3ce8e346096835fcf9cafb1943e48c1075
JperezPulgarin <jdppulga@gmail.com>
2023-06-22 11:38:26 -0500

    Initial project

---
+++ albo/src/main/java/com/challenge/albo/AlboApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.albo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlboApplication.class, args);
+	}
+
+}
